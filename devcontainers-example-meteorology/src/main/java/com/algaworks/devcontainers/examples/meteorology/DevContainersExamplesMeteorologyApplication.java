766ab097cde9c233e1cd8db8d15ad7a4f11df9cf
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-09-20 18:27:45 -0400

    feat: Add example project for test DevContainers

---
+++ devcontainers-example-meteorology/src/main/java/com/algaworks/devcontainers/examples/meteorology/DevContainersExamplesMeteorologyApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.devcontainers.examples.meteorology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevContainersExamplesMeteorologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevContainersExamplesMeteorologyApplication.class, args);
+	}
+
+}
---
+++ devcontainers-example-meteorology/src/test/java/com/algaworks/devcontainers/examples/meteorology/DevContainersExamplesMeteorologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.devcontainers.examples.meteorology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevContainersExamplesMeteorologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
