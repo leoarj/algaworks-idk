3a00231479eaf8d5722bef26cbccc6d471d0459f
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-10-11 18:45:37 -0400

    feat: Add simple PingController

---
+++ devcontainers-example-meteorology/src/main/java/com/algaworks/devcontainers/examples/meteorology/api/v1/controller/PingController.java
@@ -0,0 +1,18 @@
+package com.algaworks.devcontainers.examples.meteorology.api.v1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ping")
+public class PingController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String pong() {
+        return "pong";
+    }
+}
